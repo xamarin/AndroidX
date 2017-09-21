34f4fae6ac62ac07e20635e6b3dbace818679872
Redth <jondick@gmail.com>
2017-09-21 15:06:36 -0400

    Fix some constantly changing properties

---
+++ recyclerview-v7/source/Additions/Additions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Android.Support.V7.Widget
+                 //.RecyclerView.RequestDisallowInterceptTouchEventEventArgs
+{
+	public partial class RecyclerView
+	{
+		public partial class InterceptTouchEventEventArgs
+		{
+			public RecyclerView RecyclerView {
+				get { return this.Rv;  }
+			}
+		}
+
+		public partial class RequestDisallowInterceptTouchEventEventArgs
+		{
+			public bool Disallow
+			{
+				get { return this.DisallowIntercept; }
+			}
+		}
+
+		public partial class TouchEventEventArgs
+		{
+			public RecyclerView RecyclerView
+			{
+				get { return this.Rv; }
+			}
+		}
+	}
+}
