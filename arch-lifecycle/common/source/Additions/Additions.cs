49912730cdb410b8cd3c8886862c9ccd5e783860
Redth <jondick@gmail.com>
2017-10-06 15:15:53 -0400

    Move the lifecycle attributes
    This puts them in the Lifecycle -> Event class so they can be used like `[Lifecycle.Event.OnResume]`

--- arch-lifecycle/common/source/Additions/Additions.cs
+++ arch-lifecycle/common/source/Additions/Additions.cs
@@ -1,36 +1,39 @@
 ﻿using System;
 using Android.Runtime;
 
-namespace Lifecycle
+namespace Android.Arch.Lifecycle
 {
-	public partial class Event
+	public partial class Lifecycle
 	{
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_CREATE)")]
-		public class OnCreateAttribute : Attribute
-		{ }
+		public partial class Event
+		{
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_CREATE)")]
+			public class OnCreateAttribute : Attribute
+			{ }
 
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_START)")]
-		public class OnStartAttribute : Attribute
-		{ }
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_START)")]
+			public class OnStartAttribute : Attribute
+			{ }
 
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_RESUME)")]
-		public class OnResumeAttribute : Attribute
-		{ }
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_RESUME)")]
+			public class OnResumeAttribute : Attribute
+			{ }
 
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_PAUSE)")]
-		public class OnPauseAttribute : Attribute
-		{ }
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_PAUSE)")]
+			public class OnPauseAttribute : Attribute
+			{ }
 
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_STOP)")]
-		public class OnStopAttribute : Attribute
-		{ }
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_STOP)")]
+			public class OnStopAttribute : Attribute
+			{ }
 
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_DESTROY)")]
-		public class OnDestroyAttribute : Attribute
-		{ }
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_DESTROY)")]
+			public class OnDestroyAttribute : Attribute
+			{ }
 
-		[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_ANY)")]
-		public class OnAnyAttribute : Attribute
-		{ }
+			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Event.ON_ANY)")]
+			public class OnAnyAttribute : Attribute
+			{ }
+		}
 	}
 }
