c1cc08e259e9d1e263689d9151b62e45af7219fb
Redth <jondick@gmail.com>
2018-01-05 10:10:51 -0500

    Add NestedScrollingEnabled override to SwipeRefreshLayout
    This is the same issue as 1a6cd0b11c3b35abea3ab527e63254e9115daf40

--- support-core-ui/source/Additions/Additions.cs
+++ support-core-ui/source/Additions/Additions.cs
@@ -42,6 +42,37 @@ namespace Android.Support.V4.Widget
 
             }
         }
+
+
+        private static IntPtr id_isNestedScrollingEnabled = IntPtr.Zero;
+        private static IntPtr id_setNestedScrollingEnabled = IntPtr.Zero;
+
+        public unsafe virtual bool NestedScrollingEnabled {
+            [Register("isNestedScrollingEnabled", "()Z", "GetIsNestedScrollingEnabledHandler")]
+            get {
+                if (id_isNestedScrollingEnabled == IntPtr.Zero) {
+                    id_isNestedScrollingEnabled = JNIEnv.GetMethodID(class_ref, "isNestedScrollingEnabled", "()Z");
+                }
+                if (base.GetType() == ThresholdType) {
+                    return JNIEnv.CallBooleanMethod(base.Handle, id_isNestedScrollingEnabled);
+                }
+                return JNIEnv.CallNonvirtualBooleanMethod(base.Handle, ThresholdClass, JNIEnv.GetMethodID(ThresholdClass, "isNestedScrollingEnabled", "()Z"));
+            }
+
+            [Register("setNestedScrollingEnabled", "(Z)V", "GetSetNestedScrollingEnabledHandler")]
+            set {
+                if (id_setNestedScrollingEnabled == IntPtr.Zero) {
+                    id_setNestedScrollingEnabled = JNIEnv.GetMethodID(class_ref, "setNestedScrollingEnabled", "(Z)V");
+                }
+                JValue* ptr = stackalloc JValue[1];
+                *ptr = new JValue(value);
+                if (base.GetType() == ThresholdType) {
+                    JNIEnv.CallVoidMethod(base.Handle, id_setNestedScrollingEnabled, ptr);
+                    return;
+                }
+                JNIEnv.CallNonvirtualVoidMethod(base.Handle, ThresholdClass, JNIEnv.GetMethodID(ThresholdClass, "setNestedScrollingEnabled", "(Z)V"), ptr);
+            }
+        }
     }
 }
 
