c004d4eaabac7d3912c369f22671cc588c7dfe26
redth <jondick@gmail.com>
2019-03-06 09:59:03 -0500

    Remove some potentially unnecessary additions

--- source/androidx.leanback/leanback/additions/GuidedActionAdapter.cs
+++ source/androidx.leanback/leanback/additions/GuidedActionAdapter.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿// using System;
 
-namespace AndroidX.Leanback.Widget
-{
-    public partial class GuidedActionAdapter
-    {
-        public override void OnBindViewHolder(Java.Lang.Object holder, int position)
-        {
-            this.OnBindViewHolder((global::Android.Views.ViewGroup)holder, position);
+// namespace AndroidX.Leanback.Widget
+// {
+//     public partial class GuidedActionAdapter
+//     {
+//         public override void OnBindViewHolder(Java.Lang.Object holder, int position)
+//         {
+//             this.OnBindViewHolder((global::Android.Views.ViewGroup)holder, position);
 
-            return;
-        }
-    }
-}
+//             return;
+//         }
+//     }
+// }
--- source/androidx.preference/preference/additions/PreferenceGroupAdapter.cs
+++ source/androidx.preference/preference/additions/PreferenceGroupAdapter.cs
@@ -1,25 +1,25 @@
-﻿using System;
+﻿// using System;
 
-namespace AndroidX.Preference
-{
-    public partial class PreferenceGroupAdapter
-    {
+// namespace AndroidX.Preference
+// {
+//     public partial class PreferenceGroupAdapter
+//     {
 
-        //public void OnBindViewHolder(global::AndroidX.RecyclerView.Widget.RecyclerView.ViewHolder holder, int position)
-        //{
-        //    this.OnBindViewHolder((global::Java.Lang.Object)holder, position);
-        //}
+//         //public void OnBindViewHolder(global::AndroidX.RecyclerView.Widget.RecyclerView.ViewHolder holder, int position)
+//         //{
+//         //    this.OnBindViewHolder((global::Java.Lang.Object)holder, position);
+//         //}
 
-        public override void OnBindViewHolder (Java.Lang.Object holder, int position)
-        {
-            this.OnBindViewHolder((global::Android.Views.ViewGroup)holder, position);
+//         public override void OnBindViewHolder (Java.Lang.Object holder, int position)
+//         {
+//             this.OnBindViewHolder((global::Android.Views.ViewGroup)holder, position);
 
-            return;
-        }
+//             return;
+//         }
 
-        public Java.Lang.Object OnCreateViewHolder (Java.Lang.Object holder, int position)
-        {
-            return  this.OnCreateViewHolder((global::Android.Views.ViewGroup)holder, position);
-        }
-    }
-}
+//         public Java.Lang.Object OnCreateViewHolder (Java.Lang.Object holder, int position)
+//         {
+//             return  this.OnCreateViewHolder((global::Android.Views.ViewGroup)holder, position);
+//         }
+//     }
+// }
