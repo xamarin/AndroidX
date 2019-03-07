82f04ad86ea96d7aa838cf65197ec7505abfc5ef
redth <jondick@gmail.com>
2019-03-06 21:04:19 -0500

    Cleaned up old migrated buildtasks

--- source/buildtasks/annotations/NugetPackages.cs
+++ source/buildtasks/annotations/NugetPackages.cs
@@ -12,7 +12,7 @@ using System.Xml.Linq;
 using System.Xml.XPath;
 using Microsoft.Build.Utilities;
 
-namespace Xamarin.Android.Support.BuildTasks
+namespace Xamarin.AndroidX.Annotation.BuildTasks
 {
 	public static class NugetPackages
 	{
---
+++ source/buildtasks/annotations/VerifyVersionsTask.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Text.RegularExpressions;
+using System.Runtime.Versioning;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Text;
+
+namespace Xamarin.AndroidX.Annotation.BuildTasks
+{
+	public class VerifyVersionsTask : Task
+	{
+		public readonly static string[] ExcludedAndroidXPackages = { };
+		public readonly static string[] ExcludedAndroidSupportPackages = { };
+
+		[Required]
+		public ITaskItem ProjectPath { get; set; }
+
+		[Required]
+		public ITaskItem ProjectExtensionsPath { get; set; }
+
+		[Required]
+		public ITaskItem TargetFrameworkVersion { get; set; }
+
+		[Required]
+		public ITaskItem TargetApiLevel { get; set; }
+
+		[Required]
+		public ITaskItem AndroidSdkBuildToolsVersion { get; set; }
+
+		readonly Dictionary<string, string> androidxPackages = new Dictionary<string, string>();
+		readonly Dictionary<string, string> androidSupportPackages = new Dictionary<string, string>();
+
+		public override bool Execute()
+		{
+			Log.LogMessage("ProjectPath: {0}", ProjectPath.ItemSpec);
+			Log.LogMessage("ProjectExtensionsPath: {0}", ProjectExtensionsPath.ItemSpec);
+			Log.LogMessage("TargetFrameworkVersion: {0}", TargetFrameworkVersion.ItemSpec);
+
+			var frameworkVersion = Version.Parse(TargetFrameworkVersion.ItemSpec.ToLowerInvariant().TrimStart('v'));
+			var apiLevel = NugetPackages.GetMajorVersion(TargetApiLevel.ItemSpec);
+			var buildToolsApiLevel = NugetPackages.GetMajorVersion(AndroidSdkBuildToolsVersion.ItemSpec);
+
+			Log.LogMessage("TargetApiLevel: {0}", apiLevel);
+			Log.LogMessage("FrameworkVersion: {0}", frameworkVersion);
+			Log.LogMessage("AndroidSdkBuildToolsApiLevel: {0}", buildToolsApiLevel);
+
+			if (File.Exists(Path.Combine(ProjectPath.ItemSpec, "packages.config")))
+			{
+				Log.LogMessage("PackageReferenceType: packages.config");
+				NugetPackages.GatherPackagesConfigVersions("xamarin.androidx.", ProjectPath.ItemSpec, ExcludedAndroidXPackages, androidxPackages);
+				NugetPackages.GatherPackagesConfigVersions("xamarin.android.support.", ProjectPath.ItemSpec, ExcludedAndroidSupportPackages, androidSupportPackages);
+			}
+			else
+			{
+				Log.LogMessage("PackageReferenceType: PackageReference");
+				NugetPackages.GatherProjectJsonVersions("xamarin.androidx.", ProjectExtensionsPath.ItemSpec, ExcludedAndroidXPackages, frameworkVersion, androidxPackages, Log);
+				NugetPackages.GatherProjectJsonVersions("xamarin.android.support.", ProjectExtensionsPath.ItemSpec, ExcludedAndroidSupportPackages, frameworkVersion, androidSupportPackages, Log);
+			}
+
+			foreach (var pkgId in packageVersions.Keys)
+				Log.LogMessage("Referenced AndroidX Package: {0} ({1})", pkgId, packageVersions[pkgId]);
+
+			/* 
+			 * In the future we may add some logic to detect if certain Android Support packages
+			 * are referenced in the project but their equivalent AndroidX packages are not
+			 */
+
+			return true;
+		}
+	}
+}
