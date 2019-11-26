e44e2f61f97068f9b1672c8461ce453159d4604a
moljac <mcvjetko@holisticware.net>
2019-11-26 12:46:08 +0100

    old packagename fix

--- source/androidx.lifecycle/lifecycle-common/additions/Additions.cs
+++ source/androidx.lifecycle/lifecycle-common/additions/Additions.cs
@@ -7,31 +7,31 @@ namespace AndroidX.Lifecycle
 	{
 		public partial class Event
 		{
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_CREATE)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_CREATE)")]
 			public class OnCreateAttribute : Attribute
 			{ }
 
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_START)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_START)")]
 			public class OnStartAttribute : Attribute
 			{ }
 
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_RESUME)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_RESUME)")]
 			public class OnResumeAttribute : Attribute
 			{ }
 
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_PAUSE)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_PAUSE)")]
 			public class OnPauseAttribute : Attribute
 			{ }
 
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_STOP)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_STOP)")]
 			public class OnStopAttribute : Attribute
 			{ }
 
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_DESTROY)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_DESTROY)")]
 			public class OnDestroyAttribute : Attribute
 			{ }
 
-			[Annotation("android.arch.lifecycle.OnLifecycleEvent(android.arch.lifecycle.Lifecycle.Event.ON_ANY)")]
+			[Annotation("androidx.lifecycle.OnLifecycleEvent(androidx.lifecycle.Lifecycle.Event.ON_ANY)")]
 			public class OnAnyAttribute : Attribute
 			{ }
 		}
