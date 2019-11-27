65fa9864c6bed9fd894f2d1b1073d4fdc67ff9b3
Matthew Leibowitz <mattleibow@live.com>
2019-11-27 22:00:18 +0200

    Update more things

--- mappings/gen-nuget-csv.ps1
+++ mappings/gen-nuget-csv.ps1
@@ -8,26 +8,37 @@ $supportJson = (Invoke-WebRequest $supportConfigUrl | ConvertFrom-Json)[0]
 $androidxJson = (Get-Content "../config.json" | ConvertFrom-Json)[0]
 
 $assembliesContent = @(
-    "Support .NET assembly,AndroidX .NET assembly,Support NuGet,AndroidX NuGet"
+    "Support .NET assembly,AndroidX .NET assembly,Support NuGet,AndroidX NuGet,AndroidX NuGet Version"
 )
 
 $csv = (Get-Content $mappingCsvPath |
     ConvertFrom-Csv |
     Where-Object { $_."Support .NET assembly" -ne "" } |
+    Sort-Object { $_."Support .NET assembly" } |
     Select-Object -Property "Support .NET assembly","AndroidX .NET assembly" -Uniq)
 
 foreach ($mapping in $csv) {
     $supportNuGet = ($mapping."Support .NET assembly")
     foreach ($artifact in ($supportJson[0]."artifacts")) {
-        if (($artifact."assemblyName") -eq ($mapping."Support .NET assembly")) {
+        $name = ($artifact."assemblyName")
+        if (-not $name) {
+            $name = ($artifact."nugetId")
+        }
+        if (($name) -eq ($mapping."Support .NET assembly")) {
             $supportNuGet = ($artifact."nugetId")
             break
         }
     }
     $androidxNuGet = ($mapping."AndroidX .NET assembly")
+    $androidxVersion = ""
     foreach ($artifact in ($androidxJson[0]."artifacts")) {
-        if (($artifact."assemblyName") -eq ($mapping."AndroidX .NET assembly")) {
+        $name = ($artifact."assemblyName")
+        if (-not $name) {
+            $name = ($artifact."nugetId")
+        }
+        if (($name) -eq ($mapping."AndroidX .NET assembly")) {
             $androidxNuGet = ($artifact."nugetId")
+            $androidxVersion = ($artifact."nugetVersion")
             break
         }
     }
@@ -35,7 +46,8 @@ foreach ($mapping in $csv) {
         $mapping."Support .NET assembly" + "," +
         $mapping."AndroidX .NET assembly" + "," +
         $supportNuGet + "," +
-        $androidxNuGet
+        $androidxNuGet + "," +
+        $androidxVersion
 }
 
 $assembliesContent | Set-Content $nugetCsvPath
