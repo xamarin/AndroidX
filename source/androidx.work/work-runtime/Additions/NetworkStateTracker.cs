7d73a9685e670d8bb64d561c37e8b196733644e5
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2020-02-24 09:46:08 +0100

    AndroidX.Work fixes

---
+++ source/androidx.work/work-runtime/Additions/NetworkStateTracker.cs
@@ -0,0 +1,15 @@
+
+namespace AndroidX.Work.Impl.Constraints.Trackers 
+{
+	public partial class NetworkStateTracker 
+    {
+        protected override Java.Lang.Object RawInitialState
+        {
+            get
+            {
+                return this.InitialState;
+            }
+
+        }
+    }
+}
---
+++ source/androidx.work/work-runtime/Additions/StorageNotLowTracker.cs
@@ -0,0 +1,15 @@
+
+namespace AndroidX.Work.Impl.Constraints.Trackers 
+{
+	public partial class StorageNotLowTracker 
+    {
+        protected override Java.Lang.Object RawInitialState
+        {
+            get
+            {
+                return this.InitialState;
+            }
+
+        }
+    }
+}
