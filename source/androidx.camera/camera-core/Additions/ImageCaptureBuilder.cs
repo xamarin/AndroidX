bfa4ddd003ddcd9dd5034b30557be44c487c9bb0
Jonathan Pobst <jonathan.pobst@microsoft.com>
2021-01-04 10:25:49 -0600

    [camera-core] Add IExtendableBuilder to several *.Builder classes. (#218)

---
+++ source/androidx.camera/camera-core/Additions/ImageCaptureBuilder.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AndroidX.Camera.Core.Internal;
+using Java.Lang;
+
+namespace AndroidX.Camera.Core
+{
+	public partial class ImageCapture
+	{
+		public partial class Builder
+		{
+			// Explicitly implement these interface methods that have
+			// covariant return types in the public API.
+			Java.Lang.Object IExtendableBuilder.Build ()
+			{
+				return Build ();
+			}
+
+			Java.Lang.Object ITargetConfigBuilder.SetTargetClass (Class @class)
+			{
+				return SetTargetClass (@class);
+			}
+
+			Java.Lang.Object ITargetConfigBuilder.SetTargetName (string name)
+			{
+				return SetTargetName (name);
+			}
+
+			Java.Lang.Object IUseCaseEventConfigBuilder.SetUseCaseEventCallback (IEventCallback p0)
+			{
+				return SetUseCaseEventCallback (p0);
+			}
+		}
+	}
+}
