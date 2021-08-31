129ec5ffb7b0196c355aaafe1bd11eaccba69e7b
Jonathan Pobst <jonathan.pobst@microsoft.com>
2021-08-31 10:20:00 -0500

    Move Kotlin stdlib to this repository from XamarinComponents.

---
+++ source/Xamarin.Kotlin.StdLib/Additions/InterfacesFixups.cs
@@ -0,0 +1,13 @@
+﻿namespace Kotlin.Collections
+{
+	// TODO: Remove these fixes when this bug is fixed:
+	//       https://github.com/xamarin/java.interop/issues/470
+
+	partial class AbstractSetInvoker : Java.Util.ISet
+	{
+	}
+
+	partial class AbstractListInvoker : Java.Util.IList
+	{
+	}
+}
