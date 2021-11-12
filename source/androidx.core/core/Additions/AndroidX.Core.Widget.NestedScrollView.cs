7ba5d15f150d12e8bedcf6d7c2627809411fdf01
moljac <mcvjetko@holisticware.net>
2021-11-12 22:11:18 +0100

    metadata fixes

--- source/androidx.core/core/Additions/AndroidX.Core.Widget.NestedScrollView.cs
+++ source/androidx.core/core/Additions/AndroidX.Core.Widget.NestedScrollView.cs
@@ -22,6 +22,15 @@ namespace AndroidX.Core.Widget
 			return;
 		}
 
+		public void OnNestedScrollAccepted(Android.Views.View child, Android.Views.View target, int axes)
+		{
+			Android.Views.ScrollAxis scroll_axes = (Android.Views.ScrollAxis) axes;
+
+			OnStartNestedScroll(child, target, scroll_axes);
+
+			return;
+		}
+
 		public virtual bool OnStartNestedScroll (Android.Views.View child, Android.Views.View target, int axes, int @type)
 		{
 			Android.Views.ScrollAxis scroll_axes = (Android.Views.ScrollAxis) axes;
@@ -29,6 +38,11 @@ namespace AndroidX.Core.Widget
 			return OnStartNestedScroll(child, target, scroll_axes, @type);
 		}
 
+		public virtual bool OnStartNestedScroll (Android.Views.View child, Android.Views.View target, int axes)
+		{
+			Android.Views.ScrollAxis scroll_axes = (Android.Views.ScrollAxis) axes;
 
+			return OnStartNestedScroll(child, target, scroll_axes);
+		}
 	}
 }
