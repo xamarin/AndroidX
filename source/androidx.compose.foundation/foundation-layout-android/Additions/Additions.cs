221377488821e136aba09ed47c8aa7fe25d8ddb9
moljac <mcvjetko@holisticware.net>
2023-08-19 10:34:13 +0200

    foundation-layout-android metadata

---
+++ source/androidx.compose.foundation/foundation-layout-android/Additions/Additions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace AndroidX.Compose.Foundation.Layout 
+{
+
+	// Metadata.xml XPath class reference: path="/api/package[@name='androidx.compose.foundation.layout']/class[@name='PaddingValues.Absolute']"
+	// [global::Android.Runtime.Register ("androidx/compose/foundation/layout/PaddingValues$Absolute", DoNotGenerateAcw=true)]
+	public sealed partial class PaddingValuesAbsolute //: global::Java.Lang.Object, global::AndroidX.Compose.Foundation.Layout.IPaddingValues 
+    {
+		public unsafe float CalculateBottomPadding()
+		{
+            return this.CalculateBottomPadding_D9Ej5fM();
+		}
+
+		public unsafe float CalculateTopPadding()
+		{
+            return this.CalculateTopPadding_D9Ej5fM();
+		}
+    }
+}
+
