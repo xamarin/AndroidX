58267067ff43564f96e2291bdc275761a2973ed9
moljac <mcvjetko@holisticware.net>
2023-08-24 17:10:44 +0200

    commented out

--- source/androidx.compose.foundation/foundation-layout-android/Additions/Additions.cs
+++ source/androidx.compose.foundation/foundation-layout-android/Additions/Additions.cs
@@ -8,8 +8,8 @@ namespace AndroidX.Compose.Foundation.Layout
 
 	// Metadata.xml XPath class reference: path="/api/package[@name='androidx.compose.foundation.layout']/class[@name='PaddingValues.Absolute']"
 	// [global::Android.Runtime.Register ("androidx/compose/foundation/layout/PaddingValues$Absolute", DoNotGenerateAcw=true)]
-	public sealed partial class PaddingValuesAbsolute //: global::Java.Lang.Object, global::AndroidX.Compose.Foundation.Layout.IPaddingValues 
-    {
+	// public sealed partial class PaddingValuesAbsolute //: global::Java.Lang.Object, global::AndroidX.Compose.Foundation.Layout.IPaddingValues 
+    // {
 		// public unsafe float CalculateBottomPadding()
 		// {
         //     return this.CalculateBottomPadding_D9Ej5fM();
@@ -19,6 +19,6 @@ namespace AndroidX.Compose.Foundation.Layout
 		// {
         //     return this.CalculateTopPadding_D9Ej5fM();
 		// }
-    }
+    // }
 }
 
