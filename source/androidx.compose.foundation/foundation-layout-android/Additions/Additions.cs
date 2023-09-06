b92a6e07b895c0c3b5e8960f0508a54391361a91
moljac <mcvjetko@holisticware.net>
2023-09-06 11:41:57 +0200

    cleanup

--- source/androidx.compose.foundation/foundation-layout-android/Additions/Additions.cs
+++ source/androidx.compose.foundation/foundation-layout-android/Additions/Additions.cs
@@ -5,20 +5,5 @@ using Java.Interop;
 
 namespace AndroidX.Compose.Foundation.Layout 
 {
-
-	// Metadata.xml XPath class reference: path="/api/package[@name='androidx.compose.foundation.layout']/class[@name='PaddingValues.Absolute']"
-	// [global::Android.Runtime.Register ("androidx/compose/foundation/layout/PaddingValues$Absolute", DoNotGenerateAcw=true)]
-	// public sealed partial class PaddingValuesAbsolute //: global::Java.Lang.Object, global::AndroidX.Compose.Foundation.Layout.IPaddingValues 
-    // {
-		// public unsafe float CalculateBottomPadding()
-		// {
-        //     return this.CalculateBottomPadding_D9Ej5fM();
-		// }
-
-		// public unsafe float CalculateTopPadding()
-		// {
-        //     return this.CalculateTopPadding_D9Ej5fM();
-		// }
-    // }
 }
 
