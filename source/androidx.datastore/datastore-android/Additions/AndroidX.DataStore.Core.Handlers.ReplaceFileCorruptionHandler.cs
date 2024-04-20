808386dbda572c21fc6a9d65100f9324c64f7baf
moljac <mcvjetko@holisticware.net>
2024-04-21 00:10:52 +0200

    ReplaceFileCorruptionHandler defined in multiple assemblies

---
+++ source/androidx.datastore/datastore-android/Additions/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
@@ -0,0 +1,22 @@
+/*
+generated/androidx.datastore.datastore-core-jvm/obj/Release/net7.0-android/generated/src/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
+generated/androidx.datastore.datastore-core-jvm/obj/Release/net6.0-android/generated/src/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
+generated/androidx.datastore.datastore-core-jvm/obj/Release/monoandroid12.0/generated/src/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
+generated/androidx.datastore.datastore-core-android/obj/Release/net7.0-android/generated/src/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
+generated/androidx.datastore.datastore-core-android/obj/Release/net6.0-android/generated/src/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
+generated/androidx.datastore.datastore-core-android/obj/Release/monoandroid12.0/generated/src/AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler.cs
+*/
+
+namespace AndroidX.DataStore.Core.Internal
+{
+    /// <summary>
+    /// if AndroidX.DataStore.Core.IDataMigration ever gets removed there will be build error
+    /// 
+    /// AndroidX.DataStore.Core.IDataMigration is currently removed in androidx.datastore:datastore-core-jvm
+    /// </summary> <summary>
+    /// 
+    /// </summary>
+    internal partial interface ReplaceFileCorruptionHandler : AndroidX.DataStore.Core.Handlers.ReplaceFileCorruptionHandler
+    {
+    }
+}
