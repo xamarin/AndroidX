c3c7d31d3f360f42b2ad20bbacdb53bc361ea391
moljac <mcvjetko@holisticware.net>
2024-04-20 23:34:39 +0200

    more types defined in multiple assemblies fixes

---
+++ source/androidx.datastore/datastore-android/Additions/AndroidX.DataStore.Core.ISerializer.cs
@@ -0,0 +1,28 @@
+/*
+generated/androidx.datastore.datastore-core-jvm/obj/Release/net7.0-android/generated/src/AndroidX.DataStore.Core.ISerializer.cs
+generated/androidx.datastore.datastore-core-jvm/obj/Release/net6.0-android/generated/src/AndroidX.DataStore.Core.ISerializer.cs
+generated/androidx.datastore.datastore-core-jvm/obj/Release/monoandroid12.0/generated/src/AndroidX.DataStore.Core.ISerializer.cs
+generated/androidx.datastore.datastore-core-android/obj/Release/net7.0-android/generated/src/AndroidX.DataStore.Core.ISerializer.cs
+generated/androidx.datastore.datastore-core-android/obj/Release/net6.0-android/generated/src/AndroidX.DataStore.Core.ISerializer.cs
+generated/androidx.datastore.datastore-core-android/obj/Release/monoandroid12.0/generated/src/AndroidX.DataStore.Core.ISerializer.cs
+generated/org.jetbrains.kotlinx.kotlinx-serialization-core-jvm/obj/Release/net7.0-android/generated/src/KotlinX.Serialization.ISerializer.cs
+generated/org.jetbrains.kotlinx.kotlinx-serialization-core-jvm/obj/Release/net7.0-android/generated/src/KotlinX.Serialization.Modules.ISerializersModuleCollector.cs
+generated/org.jetbrains.kotlinx.kotlinx-serialization-core-jvm/obj/Release/net6.0-android/generated/src/KotlinX.Serialization.ISerializer.cs
+generated/org.jetbrains.kotlinx.kotlinx-serialization-core-jvm/obj/Release/net6.0-android/generated/src/KotlinX.Serialization.Modules.ISerializersModuleCollector.cs
+generated/org.jetbrains.kotlinx.kotlinx-serialization-core-jvm/obj/Release/monoandroid12.0/generated/src/KotlinX.Serialization.ISerializer.cs
+generated/org.jetbrains.kotlinx.kotlinx-serialization-core-jvm/obj/Release/monoandroid12.0/generated/src/KotlinX.Serialization.Modules.ISerializersModuleCollector.cs
+*/
+
+namespace AndroidX.DataStore.Core.Internal
+{
+    /// <summary>
+    /// if AndroidX.DataStore.Core.ISerializer ever gets removed there will be build error
+    /// 
+    /// AndroidX.DataStore.Core.ISerializer is currently removed in androidx.datastore:datastore-core-jvm
+    /// </summary> <summary>
+    /// 
+    /// </summary>
+    internal partial interface ISerializer : AndroidX.DataStore.Core.ISerializer
+    {
+    }
+}
