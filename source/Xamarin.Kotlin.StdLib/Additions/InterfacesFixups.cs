27d284291ef88eb08b470e30fde9b68eae30cfe1
moljac <mcvjetko@holisticware.net>
2024-06-17 15:00:28 +0200

    Update InterfacesFixups.cs

--- source/Xamarin.Kotlin.StdLib/Additions/InterfacesFixups.cs
+++ source/Xamarin.Kotlin.StdLib/Additions/InterfacesFixups.cs
@@ -24,5 +24,12 @@ namespace Kotlin.Collections.Builders
 
 		global::System.Collections.ICollection IMap.Values () => Values;
 	}
+
+	public partial class MapBuilderEntries
+	{
+		public override bool Add (Object? element) => Add ((IMapEntry) element!);
+
+		public override int GetSize () => Size;
+	}
 }
 #endif
