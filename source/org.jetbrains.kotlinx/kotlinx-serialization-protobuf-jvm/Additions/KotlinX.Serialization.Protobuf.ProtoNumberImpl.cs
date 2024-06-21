a695e3033f23a2a4bfccfcf1bb0fb6737df6cc01
moljac <mcvjetko@holisticware.net>
2024-06-21 14:19:26 +0200

    metadata fixes

---
+++ source/org.jetbrains.kotlinx/kotlinx-serialization-protobuf-jvm/Additions/KotlinX.Serialization.Protobuf.ProtoNumberImpl.cs
@@ -0,0 +1,16 @@
+namespace KotlinX.Serialization.Protobuf 
+{
+
+	public sealed partial class ProtoNumberImpl
+    {
+		public global::Java.Lang.Class? AnnotationType ()
+		{
+			return default(global::Java.Lang.Class);
+		}
+
+		public int Number ()
+		{
+			return default(int);
+		}
+    }
+}
---
+++ source/org.jetbrains.kotlinx/kotlinx-serialization-protobuf-jvm/Additions/KotlinX.Serialization.Protobuf.ProtoPackedImpl.cs
@@ -0,0 +1,12 @@
+namespace KotlinX.Serialization.Protobuf 
+{
+
+	public sealed partial class ProtoPackedImpl
+    {
+		public global::Java.Lang.Class? AnnotationType ()
+		{
+			return default(global::Java.Lang.Class);
+		}
+
+    }
+}
