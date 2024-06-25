22e2b7801ee90903894cb8495e7bd43acef57e99
moljac <mcvjetko@holisticware.net>
2024-06-25 19:13:38 +0200

    formatting fixed according to review

--- source/org.jetbrains.kotlinx/kotlinx-serialization-protobuf-jvm/Additions/KotlinX.Serialization.Protobuf.ProtoPackedImpl.cs
+++ source/org.jetbrains.kotlinx/kotlinx-serialization-protobuf-jvm/Additions/KotlinX.Serialization.Protobuf.ProtoPackedImpl.cs
@@ -1,12 +1,12 @@
-namespace KotlinX.Serialization.Protobuf 
+namespace KotlinX.Serialization.Protobuf
 {
 
 	public sealed partial class ProtoPackedImpl
-    {
-		public global::Java.Lang.Class? AnnotationType ()
+	{
+		public global::Java.Lang.Class? AnnotationType()
 		{
 			return default(global::Java.Lang.Class);
 		}
 
-    }
+	}
 }
--- source/org.jetbrains.kotlinx/kotlinx-serialization-protobuf-jvm/Additions/KotlinX.Serialization.Protobuf.ProtoTypeImpl.cs
+++ source/org.jetbrains.kotlinx/kotlinx-serialization-protobuf-jvm/Additions/KotlinX.Serialization.Protobuf.ProtoTypeImpl.cs
@@ -1,16 +1,16 @@
-namespace KotlinX.Serialization.Protobuf 
+namespace KotlinX.Serialization.Protobuf
 {
 
 	public sealed partial class ProtoTypeImpl
-    {
-		public global::Java.Lang.Class? AnnotationType ()
+	{
+		public global::Java.Lang.Class? AnnotationType()
 		{
 			return default(global::Java.Lang.Class);
 		}
 
- 		public KotlinX.Serialization.Protobuf.ProtoIntegerType? Type ()
+		public KotlinX.Serialization.Protobuf.ProtoIntegerType? Type()
 		{
 			return default(KotlinX.Serialization.Protobuf.ProtoIntegerType);
 		}
-    }
+	}
 }
