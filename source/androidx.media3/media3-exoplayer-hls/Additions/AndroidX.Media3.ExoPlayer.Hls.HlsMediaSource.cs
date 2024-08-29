26528232d65a5ca96daa144fe4e2aa3e7e01356f
moljac <mcvjetko@holisticware.net>
2024-08-29 22:50:03 +0200

    more metadata fixes

---
+++ source/androidx.media3/media3-exoplayer-hls/Additions/AndroidX.Media3.ExoPlayer.Hls.HlsMediaSource.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+
+namespace AndroidX.Media3.ExoPlayer.Hls;
+
+
+// Metadata.xml XPath class reference: path="/api/package[@name='androidx.media3.exoplayer.hls']/class[@name='HlsMediaSource']"
+// [global::Android.Runtime.Register ("androidx/media3/exoplayer/hls/HlsMediaSource", DoNotGenerateAcw=true)]
+public sealed partial class HlsMediaSource // : global::AndroidX.Media3.ExoPlayer.Source.BaseMediaSource, global::AndroidX.Media3.ExoPlayer.Hls.Playlist.IHlsPlaylistTrackerPrimaryPlaylistListener 
+{
+    // Metadata.xml XPath method reference: path="/api/package[@name='androidx.media3.exoplayer.hls']/class[@name='HlsMediaSource']/method[@name='canUpdateMediaItem' and count(parameter)=1 and parameter[1][@type='androidx.media3.common.MediaItem']]"
+    [Register ("canUpdateMediaItem", "(Landroidx/media3/common/MediaItem;)Z", "")]
+    public /*virtual*/ unsafe bool CanUpdateMediaItem (global::AndroidX.Media3.Common.MediaItem? mediaItem)
+    {
+        const string __id = "canUpdateMediaItem.(Landroidx/media3/common/MediaItem;)Z";
+        try {
+            JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+            __args [0] = new JniArgumentValue ((mediaItem == null) ? IntPtr.Zero : ((global::Java.Lang.Object) mediaItem).Handle);
+            var __rm = _members.InstanceMethods.InvokeAbstractBooleanMethod (__id, this, __args);
+            return __rm;
+        } finally {
+            global::System.GC.KeepAlive (mediaItem);
+        }
+    }
+
+    // Metadata.xml XPath method reference: path="/api/package[@name='androidx.media3.exoplayer.hls']/class[@name='HlsMediaSource']/method[@name='updateMediaItem' and count(parameter)=1 and parameter[1][@type='androidx.media3.common.MediaItem']]"
+    [Register ("updateMediaItem", "(Landroidx/media3/common/MediaItem;)V", "")]
+    public /*virtual*/ unsafe void UpdateMediaItem (global::AndroidX.Media3.Common.MediaItem? mediaItem)
+    {
+        const string __id = "updateMediaItem.(Landroidx/media3/common/MediaItem;)V";
+        try {
+            JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+            __args [0] = new JniArgumentValue ((mediaItem == null) ? IntPtr.Zero : ((global::Java.Lang.Object) mediaItem).Handle);
+            _members.InstanceMethods.InvokeAbstractVoidMethod (__id, this, __args);
+        } finally {
+            global::System.GC.KeepAlive (mediaItem);
+        }
+    }
+}
---
+++ source/androidx.media3/media3-exoplayer-rtsp/Additions/AndroidX.Media3.ExoPlayer.Rtsp.RtspMediaSource.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Java.Interop;
+
+namespace AndroidX.Media3.ExoPlayer.Rtsp;
+
+// Metadata.xml XPath class reference: path="/api/package[@name='androidx.media3.exoplayer.rtsp']/class[@name='RtspMediaSource']"
+// [global::Android.Runtime.Register ("androidx/media3/exoplayer/rtsp/RtspMediaSource", DoNotGenerateAcw=true)]
+public sealed partial class RtspMediaSource //: global::AndroidX.Media3.ExoPlayer.Source.BaseMediaSource 
+{
+    // Metadata.xml XPath method reference: path="/api/package[@name='androidx.media3.exoplayer.rtsp']/class[@name='RtspMediaSource']/method[@name='canUpdateMediaItem' and count(parameter)=1 and parameter[1][@type='androidx.media3.common.MediaItem']]"
+    [Register ("canUpdateMediaItem", "(Landroidx/media3/common/MediaItem;)Z", "")]
+    public /*virtual*/ unsafe bool CanUpdateMediaItem (global::AndroidX.Media3.Common.MediaItem? mediaItem)
+    {
+        const string __id = "canUpdateMediaItem.(Landroidx/media3/common/MediaItem;)Z";
+        try {
+            JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+            __args [0] = new JniArgumentValue ((mediaItem == null) ? IntPtr.Zero : ((global::Java.Lang.Object) mediaItem).Handle);
+            var __rm = _members.InstanceMethods.InvokeAbstractBooleanMethod (__id, this, __args);
+            return __rm;
+        } finally {
+            global::System.GC.KeepAlive (mediaItem);
+        }
+    }
+
+    // Metadata.xml XPath method reference: path="/api/package[@name='androidx.media3.exoplayer.rtsp']/class[@name='RtspMediaSource']/method[@name='updateMediaItem' and count(parameter)=1 and parameter[1][@type='androidx.media3.common.MediaItem']]"
+    [Register ("updateMediaItem", "(Landroidx/media3/common/MediaItem;)V", "")]
+    public /*virtual*/ unsafe void UpdateMediaItem (global::AndroidX.Media3.Common.MediaItem? mediaItem)
+    {
+        const string __id = "updateMediaItem.(Landroidx/media3/common/MediaItem;)V";
+        try {
+            JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+            __args [0] = new JniArgumentValue ((mediaItem == null) ? IntPtr.Zero : ((global::Java.Lang.Object) mediaItem).Handle);
+            _members.InstanceMethods.InvokeAbstractVoidMethod (__id, this, __args);
+        } finally {
+            global::System.GC.KeepAlive (mediaItem);
+        }
+    }
+
+}
