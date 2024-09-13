8d465e83ded2a489bb2416e1cf9cfd435352574e
Jonathan Pobst <jonathan.pobst@microsoft.com>
2024-09-13 07:22:56 -1000

    ﻿Move remaining GPS packages to AndroidX repository. (#955)
    "Pause" migrating everything to a common template, and pivot to moving the remaining packages from the GPS repository to the AndroidX repository.  This should enable us to eliminate the circular dependencies between the two repositories which should help fix the majority of the https://github.com/xamarin/AndroidX/issues/764 issues going forward.
    
    Bring in the GPS and Glide templates as new "templateSet", and merge in all the build infrastructure pieces that the GPS repository had to make it build correctly.

---
+++ source/com.google.android.gms/play-services-auth/Additions/TaskExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Gms.Extensions;
+
+namespace Android.Gms.Auth.Api.SignIn
+{
+
+    public partial class GoogleSignIn
+    {
+        public static Task<GoogleSignInAccount> GetSignedInAccountFromIntentAsync (Intent intent)
+        {
+            return GetSignedInAccountFromIntent (intent).AsAsync<GoogleSignInAccount>();
+        }
+    }
+
+    public partial class GoogleSignInClient
+    {
+        public Task RevokeAccessAsync ()
+        {
+            return RevokeAccess ().AsAsync ();
+        }
+
+        public Task SignOutAsync ()
+        {
+            return SignOut ().AsAsync ();
+        }
+
+        public Task<GoogleSignInAccount> SilentSignInAsync ()
+        {
+            return SilentSignIn ().AsAsync<GoogleSignInAccount> ();
+        }
+    }
+}
