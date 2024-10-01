25d74ffacfa866b775c1d576aec8c0cca2cee190
Jonathan Pobst <jonathan.pobst@microsoft.com>
2024-10-01 11:50:10 -1000

    ﻿Run `dotnet format` on `Xamarin.AndroidBinderator`

--- util/Xamarin.AndroidBinderator/Xamarin.AndroidBinderator/Extensions.cs
+++ util/Xamarin.AndroidBinderator/Xamarin.AndroidBinderator/Extensions.cs
@@ -1,28 +1,25 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
 using MavenNet.Models;
 
-namespace AndroidBinderator
+namespace AndroidBinderator;
+
+public static class Extensions
 {
-	public static class Extensions
+	public static bool HasValue ([NotNullWhen (true)] this string? str) => !string.IsNullOrWhiteSpace (str);
+
+	public static string OrEmpty (this string? value) => value ?? string.Empty;
+
+	public static string GroupAndArtifactId (this Dependency dependency) => $"{dependency.GroupId}.{dependency.ArtifactId}";
+
+	public static bool IsCompileDependency (this Dependency dependency) => string.IsNullOrWhiteSpace (dependency.Scope) || dependency.Scope.ToLowerInvariant ().Equals ("compile");
+
+	public static bool IsRuntimeDependency (this Dependency dependency) => dependency?.Scope != null && dependency.Scope.ToLowerInvariant ().Equals ("runtime");
+
+	public static Dependency? FindParentDependency (this Project project, Dependency dependency)
 	{
-		public static bool HasValue ([NotNullWhen (true)] this string? str) => !string.IsNullOrWhiteSpace (str);
-
-		public static string OrEmpty (this string? value) => value ?? string.Empty;
-
-		public static string GroupAndArtifactId (this Dependency dependency) => $"{dependency.GroupId}.{dependency.ArtifactId}";
-
-		public static bool IsCompileDependency (this Dependency dependency) => string.IsNullOrWhiteSpace (dependency.Scope) || dependency.Scope.ToLowerInvariant ().Equals ("compile");
-
-		public static bool IsRuntimeDependency (this Dependency dependency) => dependency?.Scope != null && dependency.Scope.ToLowerInvariant ().Equals ("runtime");
-
-		public static Dependency? FindParentDependency (this Project project, Dependency dependency)
-		{
-			return project.DependencyManagement?.Dependencies?.FirstOrDefault (
-				d => d.GroupAndArtifactId () == dependency.GroupAndArtifactId () && d.Classifier != "sources");
-		}
+		return project.DependencyManagement?.Dependencies?.FirstOrDefault (
+			d => d.GroupAndArtifactId () == dependency.GroupAndArtifactId () && d.Classifier != "sources");
 	}
 }
