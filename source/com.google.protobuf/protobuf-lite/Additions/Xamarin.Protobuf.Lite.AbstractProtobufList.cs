3855cd3b789e71fbc312ad5b67f47b45c0851567
moljac <mcvjetko@holisticware.net>
2024-12-20 09:22:11 +0100

    com.google.protobuf.protobuf-lite fixes and generated code removal

---
+++ source/com.google.protobuf/protobuf-lite/Additions/Xamarin.Protobuf.Lite.AbstractProtobufList.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Xamarin.Protobuf.Lite 
+{
+
+	// Metadata.xml XPath class reference: path="/api/package[@name='com.google.protobuf']/class[@name='AbstractProtobufList']"
+	// [global::Android.Runtime.Register ("com/google/protobuf/AbstractProtobufList", DoNotGenerateAcw=true)]
+	// [global::Java.Interop.JavaTypeParameters (new string [] {"E"})]
+	public abstract partial class AbstractProtobufList // : global::Java.Util.AbstractList, global::Xamarin.Protobuf.Lite.Internal.IProtobufList 
+    {
+		public global::Xamarin.Protobuf.Lite.Internal.IProtobufList? MutableCopyWithCapacity (int p0)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
